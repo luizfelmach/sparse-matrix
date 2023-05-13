0afc24c5686c81802299ed4cfcac645184225481
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-05-13 11:09:43 -0300

    update save matrix

--- matrix/tests/save.c
+++ matrix/tests/save.c
@@ -2,20 +2,13 @@
 #include <stdio.h>
 
 int main() {
-    Matrix sm = matrix(3, 5);
-
-    int i, j;
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 5; j++) {
-            int a;
-            scanf("%d", &a);
-            matrix_set(sm, a, i, j);
-        }
+    Matrix sm = matrix_gen(10, 10);
 
     FILE *f = fopen("matrix.bin", "wb");
 
     matrix_save(sm, f);
     matrix_show_dense(sm);
+
     matrix_destroy(sm);
     fclose(f);
     return 0;
